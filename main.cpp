0ca6e4ea800edcac551199e680b115ae634b70ed
hazyevening <198599488+hazyevening@users.noreply.github.com>
2025-11-13 13:54:53 -0300

    draw ball and create collision logic with the stage

--- main.cpp
+++ main.cpp
@@ -2,24 +2,38 @@
 #include "raylib.h"
 #include <string> 
 
+
+
+
+
 int main() {
 
     std::string screen {"TITLE"};
 
     int framesCounter {};
-    
+
+    // interface variables
     constexpr int screenWidth {1920};
     constexpr int screenHeight {1080};
-
     constexpr Color txtColor {WHITE};
     constexpr Color bgColor {BLACK};
 
+    // player variables
     constexpr int playerHeight {100};
     constexpr int playerWidth {20};
-    int playerPosY {((screenHeight / 2) - playerHeight)};
-    constexpr int playerPosX {30};    
-    constexpr int playerSpeed {10};    
-    constexpr Color playerColor {WHITE};
+    constexpr int playerPosX {100};    
+    int playerPosY {(screenHeight / 2) - playerHeight};    
+    constexpr int playerSpeed {25};    
+    constexpr Color playerColor {WHITE}; 
+
+    // ball variables
+    constexpr int ballWidth {15};
+    constexpr int ballHeight {15};
+    int ballPosX {(screenWidth / 2) - ballWidth};
+    int ballPosY {(screenHeight / 2) - ballHeight};
+    int ballSpeed {10};
+    bool invertBallDirectionV {false};
+    constexpr Color ballColor {WHITE};
     
     InitWindow(screenWidth, screenHeight, "raylib test");
     SetTargetFPS(60);    
@@ -35,6 +49,15 @@ int main() {
         }
         else if (screen == "GAME") {
             ++framesCounter;
+
+            ballPosX -= ballSpeed;
+            if (!invertBallDirectionV) {
+                ballPosY += ballSpeed;
+            }
+            else {
+                ballPosY -= ballSpeed;
+            }
+
             // Detect player input
             if (IsKeyDown(KEY_DOWN)) {
                 playerPosY += playerSpeed;
@@ -71,8 +94,24 @@ int main() {
             if ((playerPosY + playerHeight) >= screenHeight) {
                 playerPosY = screenHeight - playerHeight;
             }
+
+            // Reset ball position if out of vertical bounds
+            if (ballPosY <= 0) {
+                ballPosY = 0;
+                invertBallDirectionV = !invertBallDirectionV;
+            }
+            if (ballPosY + ballHeight >= screenHeight) {
+                ballPosY = screenHeight - ballHeight;
+                invertBallDirectionV = !invertBallDirectionV;
+            }
+            // Reset screen state if ball goes out of horizontal bounds
+            if (ballPosX <= 0 || (ballPosX + ballWidth) >= screenWidth){
+                ballPosX = (screenWidth / 2) - ballWidth;
+                ballPosY = (screenHeight / 2) - ballHeight;
+            }
  
             DrawRectangle(playerPosX, playerPosY, playerWidth, playerHeight, playerColor);
+            DrawRectangle(ballPosX, ballPosY, ballWidth, ballHeight, ballColor);
         }        
         else if (screen == "ENDING") {
             ClearBackground(bgColor);
