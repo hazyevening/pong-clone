5d4ab2daf56e83baa428f99e35c591dca4426df3
hazyevening <198599488+hazyevening@users.noreply.github.com>
2025-11-30 13:14:52 -0300

    move user defined functions back to main (issue with namespace conflicts)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,134 @@
 #include <iostream>
 #include "raylib.h"
 #include "namespaces.h"
-#include "functions.h"
 #include <string> 
 
+void updateScore(bool invert) {
+    if (!invert) {
+        Player1::score ++;
+    }
+    else {
+        Player2::score ++;
+    }
+}
+
+
+void checkWinCondition() {
+    if (Player1::score >= Interface::winScore) {
+        Interface::screen = "ENDING";
+        Interface::winner = "p1";
+    }
+    else if (Player2::score >= Interface::winScore) {
+        Interface::screen = "ENDING";
+        Interface::winner = "p2";
+    }
+    else {
+        ;      
+    }
+}
+
+
+float calcBallPosX(float speed) {
+    if (Ball::invertX) {
+        speed = -(speed * (1.0f + Ball::xModifier));
+    }
+    else if (!Ball::invertX) {
+        speed *= (1.0f + Ball::xModifier);
+    }
+
+    return speed;
+}
+
+
+float calcBallPosY(float speed) {
+    if (Ball::center) {
+        speed = 0;
+    }
+    else if (Ball::invertY) {
+        speed = -(speed * (1.0f + Ball::yModifier));
+    }
+    else if (!Ball::invertY) {
+        speed *= (1.0f + Ball::yModifier);
+    }
+
+    return speed;
+}
+
+
+void collisionLogic(std::string player) {
+    Rectangle top{};
+    Rectangle centerTop{};
+    Rectangle center{};
+    Rectangle centerBottom{};
+    Rectangle bottom{};
+
+    constexpr float oneFifthHeight {Players::height / 5};
+
+    if (player == "p1") {
+        top = {Player1::x, Player1::y, Players::width, oneFifthHeight};
+        centerTop = {Player1::x, oneFifthHeight + Player1::y, Players::width, oneFifthHeight};
+        center = {Player1::x, oneFifthHeight * 2 + Player1::y, Players::width, oneFifthHeight};
+        centerBottom = {Player1::x, oneFifthHeight * 3 + Player1::y, Players::width, oneFifthHeight};
+        bottom = {Player1::x, oneFifthHeight * 4 + Player1::y, Players::width, oneFifthHeight};
+    }
+    else if (player == "p2") {
+        top = {Player2::x, Player2::y, Players::width, oneFifthHeight};
+        centerTop = {Player2::x, oneFifthHeight + Player2::y, Players::width, oneFifthHeight};
+        center = {Player2::x, oneFifthHeight * 2 + Player2::y, Players::width, oneFifthHeight};
+        centerBottom = {Player2::x, oneFifthHeight * 3 + Player2::y, Players::width, oneFifthHeight};
+        bottom = {Player2::x, oneFifthHeight * 4 + Player2::y, Players::width, oneFifthHeight};
+    }
+    else {
+        std::cerr << "wtf";
+    }      
+    
+
+    if (CheckCollisionRecs(top, Ball::bounds)) {
+        Ball::invertY = !Ball::invertY;
+        Ball::xModifier = static_cast<double>(0.05f);
+        Ball::yModifier = -static_cast<double>(0.05f);
+        Ball::center = false;
+    }
+    else if (CheckCollisionRecs(centerTop, Ball::bounds)) {
+        Ball::xModifier = static_cast<double>(0.15f);
+        Ball::yModifier = -static_cast<double>(0.15f);
+        Ball::center = false;
+    }
+    else if (CheckCollisionRecs(center, Ball::bounds)) {
+        Ball::center = true;
+    }
+    else if (CheckCollisionRecs(centerBottom, Ball::bounds)) {
+        Ball::xModifier = -static_cast<double>(0.15f);
+        Ball::yModifier = +static_cast<double>(0.15f);
+        Ball::center = false;
+    }
+    else {
+        //Ball::xModifier = -static_cast<double>(0.3f);
+        //Ball::yModifier = +static_cast<double>(0.3f);
+        Ball::invertY = !Ball::invertY;
+        Ball::xModifier = -static_cast<double>(0.05f);
+        Ball::yModifier = static_cast<double>(0.05f);
+        Ball::center = false;
+        std::cerr << "bottom" << "\n";
+    }
+    Ball::invertX = !Ball::invertX;
+}
+
+
+void updatePlayerPosition() {
+    if (IsKeyDown(KEY_S)) {
+        Player1::y += Players::speed;
+    }
+    if (IsKeyDown(KEY_W)) {
+        Player1::y -= Players::speed;
+    }
+    if (IsKeyDown(KEY_DOWN)) {
+        Player2::y += Players::speed;
+    }
+    if (IsKeyDown(KEY_UP)) {
+        Player2::y -= Players::speed;
+    }
+}
 
 int main() {
     int framesCounter {};
@@ -41,19 +166,7 @@ int main() {
             Ball::x += (calcBallPosX(Ball::speed));
             Ball::y += (calcBallPosY(Ball::speed));
 
-            // Detect input and update player position
-            if (IsKeyDown(KEY_S)) {
-                Player1::y += Players::speed;
-            }
-            if (IsKeyDown(KEY_W)) {
-                Player1::y -= Players::speed;
-            }
-            if (IsKeyDown(KEY_DOWN)) {
-                Player2::y += Players::speed;
-            }
-            if (IsKeyDown(KEY_UP)) {
-                Player2::y -= Players::speed;
-            }
+            updatePlayerPosition();
 
             // Reset paddles position if out of vertical bounds
             if (Player1::y <= 0) {
